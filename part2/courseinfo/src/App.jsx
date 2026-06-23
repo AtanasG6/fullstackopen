b2c85b4630e584a0c0fb8cd5ec00a5accd979af1
Atanas Gyulchev <gyulchev666@gmail.com>
2026-06-23 16:36:43 +0300

    Exercise 2.3: calculate total with reduce

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -19,10 +19,7 @@ const Content = ({ parts }) => {
 }
 
 const Total = ({ parts }) => {
-  let total = 0
-  parts.forEach(part => {
-    total += part.exercises
-  })
+  const total = parts.reduce((sum, part) => sum + part.exercises, 0)
 
   return (
     <p><strong>total of {total} exercises</strong></p>
